0dbfa8adaedab59f956d3efd3b56bb5ba8cfbabd
Ameer Ameen <ameerapex496@gmail.com>
2024-02-07 22:57:39 +0100

    added all html tree that was requested

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <section>
+        <main>
+          <aside>
+            <div>
+              <p>A BOOK</p>
+              <img src="" alt="" />
+            </div>
+          </aside>
+          <aside>
+            <div>
+              <img src="" alt="" />
+              <input
+                type="text"
+                placeholder="please enter what you want to search"
+              />
+              <div>
+                <button>search</button>
+                <button>favoraties</button>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>A BOOK</p>
+              <button>save as favorat</button>
+            </div>
+          </aside>
+        </main>
+      </section>
+    </>
+  )
 }
 
 export default Home
