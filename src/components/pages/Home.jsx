d56c11bcb7f12fbfabb0d7f877a0814a92393d2b
Ameer Ameen <ameerapex496@gmail.com>
2024-02-07 23:02:06 +0100

    small styling fix

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,36 +2,34 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
-      <section>
-        <main>
-          <aside>
+    <section>
+      <main>
+        <aside>
+          <div>
+            <p>A BOOK</p>
+            <img src="" alt="" />
+          </div>
+        </aside>
+        <aside>
+          <div>
+            <img src="" alt="" />
+            <input
+              type="text"
+              placeholder="please enter what you want to search"
+            />
             <div>
-              <p>A BOOK</p>
-              <img src="" alt="" />
+              <button type="button">search</button>
+              <button type="button">favoraties</button>
             </div>
-          </aside>
-          <aside>
-            <div>
-              <img src="" alt="" />
-              <input
-                type="text"
-                placeholder="please enter what you want to search"
-              />
-              <div>
-                <button>search</button>
-                <button>favoraties</button>
-              </div>
-            </div>
-            <div>
-              <img src="" alt="" />
-              <p>A BOOK</p>
-              <button>save as favorat</button>
-            </div>
-          </aside>
-        </main>
-      </section>
-    </>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>A BOOK</p>
+            <button type="button">save as favorat</button>
+          </div>
+        </aside>
+      </main>
+    </section>
   )
 }
 
