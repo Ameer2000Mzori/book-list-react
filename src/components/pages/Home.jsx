f671ee4d944ebcc7a4baab0be042ce6c3872c9da
Ameer Ameen <ameerapex496@gmail.com>
2024-02-07 23:21:55 +0100

    html tree fix + added styling to section and main

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,8 +3,20 @@ import Logo from '../icons/logo.png'
 
 const Home = () => {
   return (
-    <section>
-      <main>
+    <section className="flex flex-col h-[100vh] w-[100vw] text-center justify-center items-center bg-slate-600">
+      <main className="flex flex-row h-[80%] w-[80%] text-center justify-center items-center bg-slate-300 rounded-md">
+        <div>
+          <img className="w-[4rem] h-[4rem]" src={Logo} alt="" />
+          <input
+            type="text"
+            placeholder="please enter what you want to search"
+          />
+          <div>
+            <button type="button">search</button>
+            <button type="button">favoraties</button>
+          </div>
+        </div>
+
         <aside>
           <div>
             <p>A BOOK</p>
@@ -12,22 +24,9 @@ const Home = () => {
           </div>
         </aside>
         <aside>
-          <div>
-            <img className="w-[4rem] h-[4rem]" src={Logo} alt="" />
-            <input
-              type="text"
-              placeholder="please enter what you want to search"
-            />
-            <div>
-              <button type="button">search</button>
-              <button type="button">favoraties</button>
-            </div>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <p>A BOOK</p>
-            <button type="button">save as favorat</button>
-          </div>
+          <img src="" alt="" />
+          <p>A BOOK</p>
+          <button type="button">save as favorat</button>
         </aside>
       </main>
     </section>
