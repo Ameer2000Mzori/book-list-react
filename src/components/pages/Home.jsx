1be1900cb151da00772ea733c1dd65ec6026d013
Ameer Ameen <ameerapex496@gmail.com>
2024-02-08 02:15:53 +0100

    finished styling the whole project with tailwind css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -38,11 +38,22 @@ const Home = () => {
               <p>A BOOK</p>
             </div>
           </aside>
-          <aside className="w-[70%] h-[100%] border-l-[1px] border-t-[1px] border-gray-400">
-            <img src="" alt="" />
-            <div>
-              <p>A BOOK</p>
-              <button type="button">save as favorat</button>
+          <aside className="w-[70%] h-[100%] border-l-[1px] border-t-[1px] border-gray-400 relative">
+            <img
+              className="w-[100%] h-[100%]  bg-white border-1 object-cover "
+              src={Logo}
+              alt=""
+            />
+            <div className="absolute bottom-5 w-[100%] h-[60px] bg-slate-400 flex flex-row justify-evenly items-center">
+              <p className="flex flex-col justify-evenly text-center ietm-center h-[40px] w-[160px] bg-slate-300 rounded-mg">
+                A BOOK
+              </p>
+              <button
+                type="button"
+                className="h-[40px] w-[160px] bg-slate-500 rounded-mg hover:bg-slate-200 active:bg-slate-600"
+              >
+                save as favorat
+              </button>
             </div>
           </aside>
         </div>
