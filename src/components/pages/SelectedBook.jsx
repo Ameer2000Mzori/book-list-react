7335d65136ec7c5640c864943b52bbde460d5924
Ameer Ameen <ameerapex496@gmail.com>
2024-02-23 05:23:22 +0100

    cleaning up the code

--- src/components/pages/SelectedBook.jsx
+++ src/components/pages/SelectedBook.jsx
@@ -2,33 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types' // Make sure this line is added
 import Logo from '../icons/logo.png'
 
-export const favoList = [
-  {
-    id: 1,
-    title: 'test',
-  },
-]
-
 const SelectedBook = ({ title }) => {
-  const foundBook = favoList.some((book) => book.title === title)
-  const saveFavo = () => {
-    if (foundBook) {
-      console.log('this book is already in your favourites')
-    } else {
-      console.log('saved', title)
-      favoList.push({
-        title,
-      })
-      console.log('this is our favolist afer :', favoList)
-    }
-  }
-
-  const removeFavo = () => {
-    const index = favoList.findIndex((book) => book.title === title)
-    favoList.splice(index, 1)
-    console.log('this is our favolist afer :', favoList)
-  }
-
   return (
     <div>
       <img
@@ -42,7 +16,6 @@ const SelectedBook = ({ title }) => {
         </p>
         {foundBook ? (
           <button
-            onClick={() => removeFavo(title)}
             type="button"
             className="h-[40px] w-[160px] bg-slate-500 rounded-mg hover:bg-slate-200 active:bg-slate-600"
           >
@@ -50,7 +23,6 @@ const SelectedBook = ({ title }) => {
           </button>
         ) : (
           <button
-            onClick={() => saveFavo(title)}
             type="button"
             className="h-[40px] w-[160px] bg-slate-500 rounded-mg hover:bg-slate-200 active:bg-slate-600"
           >
